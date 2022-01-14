6fedd4ce9d0363ce3e24b18d78e6c2298409663e
truong.vx <33568957+truong-vx352@users.noreply.github.com>
2022-01-14 23:30:51 +0700

    delete folder

--- src/component/authHandler/auth-header.js
+++
@@ -1,9 +0,0 @@
-export default function authHeader() {
-    const user = localStorage.getItem('user');
-
-    if (user) {
-        return { Authorization: `Bearer ${user}` };
-    } else {
-        return {};
-    }
-}
